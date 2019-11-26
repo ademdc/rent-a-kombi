68a267099f33aef0b576118196bd25a934f70cf1
ademdc <ademdinarevic@gmail.com>
2019-11-26 23:07:52 +0100

    added tabs for editing post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
-  has_one_attached :avatar
   has_many :conversations, ->(user) { unscope(:where).where("recipient_id = :id OR sender_id = :id", id: user.id) }
   has_many :messages, through: :conversations
+
+  has_one_attached :avatar
+
 
   def admin?
     is_admin

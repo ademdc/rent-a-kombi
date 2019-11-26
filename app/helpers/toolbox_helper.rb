39ea68872f6c48b41fe65b3248342768d4f9f70c
ademdc <ademdinarevic@gmail.com>
2019-11-27 00:05:06 +0100

    added icons for tabs

--- app/helpers/toolbox_helper.rb
+++ app/helpers/toolbox_helper.rb
@@ -2,4 +2,16 @@ module ToolboxHelper
   def message_icon(klass = '')
     content_tag :i, '', class: "fa fa-envelope #{klass}"
   end
+
+  def info_icon(klass='')
+    content_tag :i, '', class: "fa fa-info #{klass}"
+  end
+
+  def camera_icon(klass='')
+    content_tag :i, '', class: "fa fa-camera-retro #{klass}"
+  end
+
+  def calendar_icon(klass='')
+    content_tag :i, '', class: "fa fa-calendar #{klass}"
+  end
 end

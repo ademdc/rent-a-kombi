74faa89504c115330554ddc3efa50ca3c88eded3
ademdc <ademdinarevic@gmail.com>
2019-12-16 19:19:08 +0100

    added fa cdn, redesign of show post

--- app/helpers/reservation_helper.rb
+++ app/helpers/reservation_helper.rb
@@ -9,7 +9,7 @@ module ReservationHelper
     content_tag :div do
       concat warning_icon('mr-2')
       concat content_tag :span, 'You have an upcoming reservation for this post'
-      concat link_to 'See reservations', profile_index_path, class: 'd-block'
+      concat link_to 'See reservations', profile_index_path, class: 'd-block mb-2'
     end
   end
 end

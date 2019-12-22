dc32d1bd3931c98eb298ea810a4abf7753c1c666
ademdc <ademdinarevic@gmail.com>
2019-12-22 18:02:48 +0100

    delete vehicle model and table, delete some table columns

---
+++ db/migrate/20191222165755_remove_unnecesary_columns.rb
@@ -0,0 +1,9 @@
+class RemoveUnnecesaryColumns < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :posts, :string
+    remove_column :users, :city
+    remove_column :users, :address
+
+    drop_table :vehicles
+  end
+end

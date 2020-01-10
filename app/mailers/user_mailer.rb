d4bd12a01b55853a4646394722cb082875435575
Adem Dinarevic <ademdinarevic@gmail.com>
2020-01-10 17:45:05 +0100

    reservation email update based on user locale

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ApplicationMailer
   def reservation_confirmation
     @reservation = params[:reservation]
-    mail(:to => @reservation.post.user.email, :subject => I18n.t('mailer.reservation.upcoming_reservation', title: @reservation.post.title))
+    @receiver = @reservation.post.user
+    @localized_subject = I18n.with_locale(@receiver.locale.to_sym) { I18n.t('mailer.reservation.upcoming_reservation', title: @reservation.post.title) }
+
+    mail(
+      :to => @receiver.email,
+      :subject => @localized_subject
+    )
   end
 end

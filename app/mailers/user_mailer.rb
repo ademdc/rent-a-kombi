b806b694106e14738500819304b15a13f88dc739
Adem Dinarevic <ademdinarevic@gmail.com>
2020-01-10 15:31:24 +0100

    added translations for mailer, fix css for mobile

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def reservation_confirmation
     @reservation = params[:reservation]
-    mail(:to => @reservation.post.user.email, :subject => "Upcoming Reservation for #{@reservation.post.title}")
+    mail(:to => @reservation.post.user.email, :subject => I18n.t('mailer.reservation.upcoming_reservation', title: @reservation.post.title))
   end
 end

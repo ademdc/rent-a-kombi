c183657f744bba5ed717f81261e5f90a49b00949
ademdc <ademdinarevic@gmail.com>
2020-06-12 00:20:20 +0200

    added first name, last name on registration

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::RegistrationsController < Api::V1::ApplicationController
   skip_before_action :require_login, only: [:create]
 
   def create
-    user = User.create(email: params[:users][:email], password: params[:users][:password])
+    user = User.create(email: params[:users][:email], password: params[:users][:password], first_name: params[:users][:first_name], last_name: params[:users][:last_name])
 
     if user.valid?
         payload = { user_id: user.id }
@@ -19,6 +19,6 @@ class Api::V1::RegistrationsController < Api::V1::ApplicationController
   protected
 
   def user_params
-    params.permit(:email, :password)
+    params.permit(:email, :password, :first_name, :last_name)
   end
 end
